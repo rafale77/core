5cd0d621220d931147f386ec9673e2ddf076da04
rafale77 <anhmantran@yahoo.com>
2020-07-29 07:27:24 -0700

    moved to cv2

--- homeassistant/components/ffmpeg/camera.py
+++ homeassistant/components/ffmpeg/camera.py
@@ -1,14 +1,14 @@
 """Support for Cameras with FFmpeg as decoder."""
 import asyncio
 import logging
+import os
 
-from haffmpeg.camera import CameraMjpeg
-from haffmpeg.tools import IMAGE_JPEG, ImageFrame
 import voluptuous as vol
+import cv2
+from threading import Thread
 
 from homeassistant.components.camera import PLATFORM_SCHEMA, SUPPORT_STREAM, Camera
 from homeassistant.const import CONF_NAME
-from homeassistant.helpers.aiohttp_client import async_aiohttp_proxy_stream
 import homeassistant.helpers.config_validation as cv
 
 from . import CONF_EXTRA_ARGUMENTS, CONF_INPUT, DATA_FFMPEG
@@ -16,26 +16,61 @@ from . import CONF_EXTRA_ARGUMENTS, CONF_INPUT, DATA_FFMPEG
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = "FFmpeg"
-DEFAULT_ARGUMENTS = "-pred 1"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_INPUT): cv.string,
-        vol.Optional(CONF_EXTRA_ARGUMENTS, default=DEFAULT_ARGUMENTS): cv.string,
+        vol.Optional(CONF_EXTRA_ARGUMENTS): cv.string,
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     }
 )
 
-
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up a FFmpeg camera."""
-    async_add_entities([FFmpegCamera(hass, config)])
+    async_add_entities([FFmpegCamera(hass, config, hass.data[DATA_FFMPEG])])
 
+class Client:
+    """ Maintain live RTSP feed without buffering. """
+    _stream = None
+
+    def __init__(self, rtsp_server_uri, extra_cmd):
+        """
+            rtsp_server_uri: the path to an RTSP server. should start with "rtsp://"
+        """
+        self.rtsp_server_uri = rtsp_server_uri
+        self.extra_cmd = extra_cmd
+        self.open()
+
+    def open(self):
+        os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "hwaccel;cuvid|video_codec;h264_cuvid|vsync;0"
+        if self.extra_cmd == "h265":
+            os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "hwaccel;cuvid|video_codec;hevc_cuvid|vsync;0"
+        self._stream = cv2.VideoCapture(self.rtsp_server_uri, cv2.CAP_FFMPEG)
+        t = Thread(target=self._update, args=())
+        t.daemon = True
+        t.start()
+        return self
+
+    def _update(self):
+        while True:
+            grabbed = self._stream.grab()
+            if not grabbed:
+                _LOGGER.warning("Stream Interrupted, Retrying")
+                self._stream.release()
+                os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "hwaccel;cuvid|video_codec;h264_cuvid|vsync;0"
+                if self.extra_cmd == "h265":
+                    os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "hwaccel;cuvid|video_codec;hevc_cuvid|vsync;0"
+                self._stream = cv2.VideoCapture(self.rtsp_server_uri, cv2.CAP_FFMPEG)
+
+    def read(self):
+        """ Retrieve most recent frame and decode"""
+        (read, frame) = self._stream.retrieve()
+        return frame
 
 class FFmpegCamera(Camera):
     """An implementation of an FFmpeg camera."""
 
-    def __init__(self, hass, config):
+    def __init__(self, hass, config, ffmpeg):
         """Initialize a FFmpeg camera."""
         super().__init__()
 
@@ -43,6 +78,8 @@ class FFmpegCamera(Camera):
         self._name = config.get(CONF_NAME)
         self._input = config.get(CONF_INPUT)
         self._extra_arguments = config.get(CONF_EXTRA_ARGUMENTS)
+        self._ffmpeg = ffmpeg
+        self.client = Client(rtsp_server_uri = self._input, extra_cmd=self._extra_arguments)
 
     @property
     def supported_features(self):
@@ -53,34 +90,20 @@ class FFmpegCamera(Camera):
         """Return the stream source."""
         return self._input.split(" ")[-1]
 
+    async def async_camera_raw_image(self):
+        """Return a still image response from the camera."""
+        frame = self.client.read()
+        return frame
+
     async def async_camera_image(self):
         """Return a still image response from the camera."""
-
-        ffmpeg = ImageFrame(self._manager.binary, loop=self.hass.loop)
-
-        image = await asyncio.shield(
-            ffmpeg.get_image(
-                self._input, output_format=IMAGE_JPEG, extra_cmd=self._extra_arguments
-            )
-        )
-        return image
+        frame = self.client.read()
+        ret, image = cv2.imencode('.jpg', frame)
+        return image.tobytes()
 
     async def handle_async_mjpeg_stream(self, request):
         """Generate an HTTP MJPEG stream from the camera."""
-
-        stream = CameraMjpeg(self._manager.binary, loop=self.hass.loop)
-        await stream.open_camera(self._input, extra_cmd=self._extra_arguments)
-
-        try:
-            stream_reader = await stream.get_reader()
-            return await async_aiohttp_proxy_stream(
-                self.hass,
-                request,
-                stream_reader,
-                self._manager.ffmpeg_stream_content_type,
-            )
-        finally:
-            await stream.close()
+        return await super().handle_async_mjpeg_stream(request)
 
     @property
     def name(self):
