17dabfcd84d6de3008746c215f549ec8998bbe3a
rafale77 <anhmantran@yahoo.com>
2020-08-20 12:53:36 -0700

    Update image_processing.py

--- homeassistant/components/dlib_face_identify/image_processing.py
+++ homeassistant/components/dlib_face_identify/image_processing.py
@@ -2,7 +2,6 @@
 import logging
 import numpy as np
 from easydict import EasyDict as edict
-#import dlib
 import cv2
 import torch
 
@@ -11,8 +10,6 @@ from .Arcface import Backbone
 
 # pylint: disable=import-error
 import voluptuous as vol
-#from sklearn import svm
-#from joblib import dump, load
 from pathlib import Path
 import os
 
@@ -27,10 +24,10 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 home = str(Path.home())+"/.homeassistant/"
-
 ATTR_NAME = "name"
 
 def get_config():
+
     conf = edict()
     conf.model_path = home+'/model/'
     conf.log_path = home
@@ -51,6 +48,7 @@ def get_config():
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Dlib Face detection platform."""
+
     entities = []
     for camera in config[CONF_SOURCE]:
         entities.append(
@@ -69,52 +67,21 @@ class DlibFaceIdentifyEntity(ImageProcessingFaceEntity):
         """Initialize Dlib face identify entry."""
 
         super().__init__()
-        self.model = "arc"
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.face_detector = FaceDetector(weight_path=home+'model/Resnet50_Final.pth', device=self.device)
         self._camera = camera_entity
-        if self.model == "arc":
-            self.conf = get_config()
-            self.arcmodel = Backbone(self.conf.net_depth, self.conf.drop_ratio, self.conf.net_mode).to(self.device)
-            try:
-                self.arcmodel.load_state_dict(torch.load(f'{self.conf.model_path}/model_ir_se50.pth'))
-            except IOError as e:
-                _LOGGER.warning("Arcface weight does not exist")
-            self.arcmodel.eval()
-        else:
-            self.face_encoder = dlib.face_recognition_model_v1(home+"model/dlib_face_recognition_resnet_model_v1.dat")
-            self.fmodel = "large"
+        self.conf = get_config()
+        self.arcmodel = Backbone(self.conf.net_depth, self.conf.drop_ratio, self.conf.net_mode).to(self.device)
+        try:
+            self.arcmodel.load_state_dict(torch.load(f'{self.conf.model_path}/model_ir_se50.pth'))
+        except IOError as e:
+            _LOGGER.warning("Arcface weight does not exist")
+        self.arcmodel.eval()
         if name:
             self._name = name
         else:
             self._name = f"Dlib Face {split_entity_id(camera_entity)[1]}"
-
         self.train_faces()
-
-
-    def _raw_face_landmarks(self, face_image, face_locations, model="large"):
-
-        face_locations = [dlib.rectangle(face_location[3], face_location[0], face_location[1], face_location[2]) for face_location in face_locations]
-        if model == "small":
-            pose_predictor = dlib.shape_predictor(home+"model/shape_predictor_5_face_landmarks.dat")
-        else:
-            pose_predictor = dlib.shape_predictor(home+"model/shape_predictor_68_face_landmarks.dat")
-        return [pose_predictor(face_image, face_location) for face_location in face_locations]
-
-
-    def face_encodings(self, face_image, known_face_locations, num_jitters=1, model="large"):
-        """
-        Given an image, return the 128-dimension face encoding for each face in the image.
-
-        :param face_image: The image that contains one or more faces
-        :param known_face_locations: Optional - the bounding boxes of each face if you already know them.
-        :param num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
-        :param model: Optional - which model to use. "large" (default) or "small" which only returns 5 points but is faster.
-        :return: A list of 128-dimensional face encodings (one for each face in the image)
-        """
-        raw_landmarks = self._raw_face_landmarks(face_image, known_face_locations, model)
-        return [np.array(self.face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks]
-
 
     def faces_preprocessing(self, faces):
 
@@ -124,37 +91,16 @@ class DlibFaceIdentifyEntity(ImageProcessingFaceEntity):
         faces[:].sub_(mu[:, None, None]).div_(mu[:, None, None])
         return faces
 
-
-    def locate(self, image):
-
-        dlibrect = []
-        (h, w) = image.shape[:2]
-        ratio = 2
-        image = cv2.resize(image, (int(w/ratio), int(h/ratio)))
-        boxes, scores, landmarks = self.face_detector.detect_faces(image)
-        if len(boxes) > 0:
-            for box in boxes:
-                face = dlib.rectangle(int(box[0]*ratio), int(box[1]*ratio), int(box[2]*ratio), int(box[3])*ratio)
-                dlibrect.append(face)
-
-        return [(max(face.top(), 0), min(face.right(), w), min(face.bottom(), h), max(face.left(), 0)) for face in dlibrect]
-
-
     def train_faces(self):
 
         try:
-            if self.model == "arc":
-                self.targets = torch.load(self.conf.facebank_path/'facebank.pth')
-                self.names = np.load(self.conf.facebank_path/'names.npy')
-            else:
-                self.clf = load(home+'model.joblib')
+            self.targets = torch.load(self.conf.facebank_path/'facebank.pth')
+            self.names = np.load(self.conf.facebank_path/'names.npy')
             _LOGGER.warning("Faces Loaded")
         except:
             _LOGGER.warning("Model not trained, retraining...")
             faces = []
-            names = []
-            if self.model == "arc":
-                names = ['Unknown']
+            names = ['Unknown']
             dir = home+"recogface/faces/"
             train_dir = os.listdir(dir)
             for person in train_dir:
@@ -162,71 +108,41 @@ class DlibFaceIdentifyEntity(ImageProcessingFaceEntity):
                 embs = []
                 for person_img in pix:
                     pic = cv2.imread(dir + person + "/" + person_img)
-                    if self.model == "arc":
-                        face = self.face_detector.detect_align(pic)[0]
-                        if len(face) == 1:
-                            with torch.no_grad():
-                                embs.append(self.arcmodel(self.faces_preprocessing(face)))
-                        else:
-                            _LOGGER.error(person_img+" can't be used for training")
+                    face = self.face_detector.detect_align(pic)[0]
+                    if len(face) == 1:
+                        with torch.no_grad():
+                            embs.append(self.arcmodel(self.faces_preprocessing(face)))
                     else:
-                        boxes = self.locate(pic)
-                        if len(boxes) == 1:
-                            face = cv2.cvtColor(pic, cv2.COLOR_BGR2RGB)
-                            face_enc = self.face_encodings(face, boxes, 100, model=self.fmodel)[0]
-                            embs.append(face_enc)
-                        else:
-                            _LOGGER.error(person_img+" can't be used for training")
-                if self.model == "arc":
-                    faces.append(torch.cat(embs).mean(0, keepdim=True))
-                    names.append(person)
-            if self.model == "arc":
-               self.targets = torch.cat(faces)
-               torch.save(self.targets, str(self.conf.facebank_path)+'/facebank.pth')
-               self.names = np.array(names)
-               np.save(str(self.conf.facebank_path)+'/names', self.names)
-            else:
-                # Create and train the SVC classifier
-                self.clf = svm.SVC(gamma ='scale')
-                self.clf.fit(faces, names)
-                dump(self.clf, home+'model.joblib')
+                        _LOGGER.error(person_img+" can't be used for training")
+                faces.append(torch.cat(embs).mean(0, keepdim=True))
+                names.append(person)
+            self.targets = torch.cat(faces)
+            torch.save(self.targets, str(self.conf.facebank_path)+'/facebank.pth')
+            self.names = np.array(names)
+            np.save(str(self.conf.facebank_path)+'/names', self.names)
             _LOGGER.warning("Model training completed and saved...")
-
 
     @property
     def camera_entity(self):
         """Return camera entity id from process pictures."""
         return self._camera
 
-
     @property
     def name(self):
         """Return the name of the entity."""
         return self._name
 
-
     def process_image(self, image):
         """Process image."""
         unknowns = []
         found = []
-        if self.model == "arc":
-            faces, unknowns, scores, landmarks = self.face_detector.detect_align(image)
-            if len(faces)>0:
-                embs = self.arcmodel(self.faces_preprocessing(faces))
-                diff = embs.unsqueeze(-1) - self.targets.transpose(1, 0).unsqueeze(0)
-                dist = torch.sum(torch.pow(diff, 2), dim=1)
-                minimum, min_idx = torch.min(dist, dim=1)
-                min_idx[minimum > self.conf.threshold] = -1  # if no match, set idx to -1
-
-                for idx, bbox in enumerate(unknowns):
-                    found.append({ATTR_NAME: self.names[min_idx[idx]+1]})
-        else:
-            face_locations = self.locate(image)
-            if face_locations:
-                im = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-                unknowns = self.face_encodings(im, face_locations, 10, model=self.fmodel)
-                for unknown_face in unknowns:
-                    name = self.clf.predict([unknown_face])
-                    name = np.array2string(name)
-                    found.append({ATTR_NAME: name})
+        faces, unknowns, scores, landmarks = self.face_detector.detect_align(image)
+        if len(faces)>0:
+            embs = self.arcmodel(self.faces_preprocessing(faces))
+            diff = embs.unsqueeze(-1) - self.targets.transpose(1, 0).unsqueeze(0)
+            dist = torch.sum(torch.pow(diff, 2), dim=1)
+            minimum, min_idx = torch.min(dist, dim=1)
+            min_idx[minimum > self.conf.threshold] = -1  # if no match, set idx to -1
+            for idx, bbox in enumerate(unknowns):
+                found.append({ATTR_NAME: self.names[min_idx[idx]+1]})
         self.process_faces(found, len(unknowns))
