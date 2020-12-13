a987f5a9de1046175c98543291e4ebf86c197568
rafale77 <anhmantran@yahoo.com>
2020-12-12 18:10:14 -0800

    Update yolo.py

--- homeassistant/components/opencv/models/yolo.py
+++ homeassistant/components/opencv/models/yolo.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import logging
 import math
 import ast
+import time
 
 import torch
 import torch.nn as nn
@@ -112,7 +113,7 @@ def model_info(model, verbose=False, imgsz=64, device="cpu"):
                     i,
                     name,
                     p.requires_grad,
-                    p.numel(), 
+                    p.numel(),
                     list(p.shape),
                     p.mean(),
                     p.std(),
@@ -125,7 +126,6 @@ def model_info(model, verbose=False, imgsz=64, device="cpu"):
 
 class Detect(nn.Module):
     stride = None  # strides computed during build
-    export = False  # onnx export
 
     def __init__(self, nc=80, anchors=(), ch=()):  # detection layer
         super().__init__()
@@ -147,7 +147,6 @@ class Detect(nn.Module):
     def forward(self, x):
         # x = x.copy()  # for profiling
         z = []  # inference output
-        self.training |= self.export
         for i in range(self.nl):
             x[i] = self.m[i](x[i])  # conv
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
@@ -157,19 +156,17 @@ class Detect(nn.Module):
                 .permute(0, 1, 3, 4, 2)
                 .contiguous()
             )
+            if self.grid[i].shape[2:4] != x[i].shape[2:4]:
+                self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
 
-            if not self.training:  # inference
-                if self.grid[i].shape[2:4] != x[i].shape[2:4]:
-                    self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
+            y = x[i].sigmoid()
+            y[..., 0:2] = (
+                y[..., 0:2] * 2. - 0.5 + self.grid[i].to(x[i].device)
+            ) * self.stride[i]  # xy
+            y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+            z.append(y.view(bs, -1, self.no))
 
-                y = x[i].sigmoid()
-                y[..., 0:2] = (
-                    y[..., 0:2] * 2. - 0.5 + self.grid[i].to(x[i].device)
-                ) * self.stride[i]  # xy
-                y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
-                z.append(y.view(bs, -1, self.no))
-
-        return x if self.training else (torch.cat(z, 1), x)
+        return (torch.cat(z, 1), x)
 
     @staticmethod
     def _make_grid(nx=20, ny=20):
@@ -186,7 +183,7 @@ class Model(nn.Module):
             self.yaml = cfg  # model dict
         else:  # is *.yaml
             import yaml  # for torch hub
-            
+
             self.yaml_file = Path(cfg).name
             with open(cfg) as f:
                 self.yaml = yaml.load(f, Loader=yaml.FullLoader)  # model dict
@@ -243,7 +240,7 @@ class Model(nn.Module):
             if m.f != -1:  # if not from previous layer
                 x = (
                     y[m.f]
-                    if isinstance(m.f, int) 
+                    if isinstance(m.f, int)
                     else [x if j == -1 else y[j] for j in m.f]
                 )  # from earlier layers
 
@@ -360,7 +357,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         else:
             c2 = ch[f]
 
-        m_ = nn.Sequential(*[m(*args) for _ in range(n)]) if n > 1 else m(*args)  
+        m_ = nn.Sequential(*[m(*args) for _ in range(n)]) if n > 1 else m(*args)
         t = str(m)[8:-2].replace('__main__.', '')  # module type
         np = sum([x.numel() for x in m_.parameters()])  # number params
         m_.i, m_.f, m_.type, m_.np = i, f, t, np
