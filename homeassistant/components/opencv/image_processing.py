2fc2b64ab9a2b3a342ae1e3088cf0a8a43dbe21e
rafale77 <anhmantran@yahoo.com>
2020-12-13 11:17:48 -0800

    Update image_processing.py

--- homeassistant/components/opencv/image_processing.py
+++ homeassistant/components/opencv/image_processing.py
@@ -58,7 +58,7 @@ def xywh2xyxy(x):
 
 
 def non_max_suppression(prediction, conf_thres=0.1, iou_thres=0.6, classes=None):
-    """detections with shape: nx6 (x1, y1, x2, y2, conf, cls)."""
+    """Returns detection with shape: nx6 (x1, y1, x2, y2, conf, cls)."""
 
     nc = prediction[0].shape[1] - 5  # number of classes
     xc = prediction[..., 4] > conf_thres  # candidates
