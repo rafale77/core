bf38ae7b654f5aff7f6be19d4e04427df88649b3
rafale77 <anhmantran@yahoo.com>
2020-12-12 19:31:09 -0800

    Update yolo.py

--- homeassistant/components/opencv/models/yolo.py
+++ homeassistant/components/opencv/models/yolo.py
@@ -282,8 +282,8 @@ class Model(nn.Module):
             b = mi.bias.view(m.na, -1)  # conv.bias(255) to (3,85)
             b[:, 4] += math.log(8 / (640 / s) ** 2)  # obj (8 objects per 640 image)
             b[:, 5:] += (
-                math.log(0.6 / (m.nc - 0.99)) 
-                if cf is None 
+                math.log(0.6 / (m.nc - 0.99))
+                if cf is None
                 else torch.log(cf / cf.sum())
             )  # cls
             mi.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
