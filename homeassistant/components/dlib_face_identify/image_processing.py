77770bcea8b91e02a52eb32abbcea7992868e70c
rafale77 <anhmantran@yahoo.com>
2020-12-23 09:17:17 -0800

    Update image_processing.py

--- homeassistant/components/dlib_face_identify/image_processing.py
+++ homeassistant/components/dlib_face_identify/image_processing.py
@@ -75,7 +75,10 @@ class DlibFaceIdentifyEntity(ImageProcessingFaceEntity):
         ).to(self.device)
         try:
             self.arcmodel.load_state_dict(
-                torch.load(f"{self.conf.model_path}/model_ir_se50.pth", map_location=self.device)
+                torch.load(
+                    f"{self.conf.model_path}/model_ir_se50.pth", 
+                    map_location=self.device,
+                )
             )
         except OSError:
             _LOGGER.warning("Arcface weight does not exist")
